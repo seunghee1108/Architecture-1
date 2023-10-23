2cb0bf573654c385b5e09e51b314f71233f964a8
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-23 23:27:10 +0900

    localhost:3000 서버 가동test

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ const contentType = {
 // 서버 생성
 const server = http.createServer(function(request, response) {
  if(request.method === 'GET' && request.url === '/') {
-  // 200:정상적으로 응답
+  // 200 코드는 정상적으로 응답했다는 의미이다.
   response.writeHead(200, contentType);
   response.end(HTMLTemplate);
  } else {
   response.writeHead(404, contentType);
+  // 404 코드와 함께 페이지를 찾을 수 없음을 알려주는 응답을 보낸다. 
   response.end('<h1>페이지 요청데이터를 찾을 수 없을 때 404에러를 응답하는 것이 HTTP 프로토콜</h1>')
  }
 });
