aa00ef78e38055cdb3d126ba7f3c6e7af889657b
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-23 20:41:38 +0900

    포켓몬스터 한글 배열 정상적으로 불러왔는지 확인하기 위해 출력해보기, 확인 완료

---
+++ model/HTML-model.js
@@ -0,0 +1,6 @@
+// 포켓몬스터 한글 배열 불러오기
+const _pokemonKoreaName = require('./pokemon-package-model');
+// li 태그 만들어주는 모듈 불러오기
+const setMakeLiTagPokemonList = require('./make-li-tag-model');
+
+console.log(_pokemonKoreaName);
