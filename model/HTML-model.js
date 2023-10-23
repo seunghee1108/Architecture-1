0f63cec381b874d6eb1cb239ee7bcfc99982c9dc
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-23 23:38:03 +0900

    주석 작성

--- model/HTML-model.js
+++ model/HTML-model.js
@@ -3,9 +3,10 @@ const _pokemonKoreaName = require('./pokemon-package-model');
 // li 태그 만들어주는 모듈 불러오기
 const setMakeLiTagPokemonList = require('./make-li-tag-model');
 
+// 포켓몬스터 한글 배열이 정상적으로 들어왔는지 확인하기 위해 출력
 console.log(_pokemonKoreaName);
-console.log(typeof(setMakeLiTagPokemonList));
-// function
+// li 태그 만들어주는 모듈이 정상적으로 들어왔는지 확인하기 위해 출력
+console.log(typeof(setMakeLiTagPokemonList)); // function
 
 const HTMLTemplate = `
 <!DOCTYPE html>
@@ -24,7 +25,14 @@ const HTMLTemplate = `
 </body>
 </html>
 `;
+
+/**
+ * 정상적으로 조립되었는지 확인
+ * HTML을 제작하는 것이긴 하나, html로 해석되기전에는 단순 문자열 조합일 뿐이다.
+ * server가 html로 해석해서 보여줄 것이다.
+ */
 console.log(HTMLTemplate);
+
 
 
 module.exports = HTMLTemplate;
