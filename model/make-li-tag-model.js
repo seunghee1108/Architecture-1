064637aa27545c69822d7f7e8cd8a3e36b9e5069
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-23 20:36:19 +0900

    tag 모듈 내보내기

--- model/make-li-tag-model.js
+++ model/make-li-tag-model.js
@@ -1,7 +1,11 @@
+// 포켓몬스터 한글 배열을 HTML li 태그로 만들어주는 함수
 function setMakeLiTagPokemonList(pokemonKoreaName) {
+  // Array.isArray():Array인지 확인한다.
+  // 객체가 Array라면 true
   if(Array.isArray(pokemonKoreaName) === true) {
     let arrayLiTagPokemonList = [];
     for(let i = 0; i < pokemonKoreaName.length; i++) {
+      // push():마지막에 새로운 요소를 추가하고 난 후 변경된 배열의 길이를 반환한다.
       arrayLiTagPokemonList.push(`<li>${pokemonKoreaName[i]}</li>`);
     } 
     return arrayLiTagPokemonList.join("");
@@ -11,3 +15,5 @@ function setMakeLiTagPokemonList(pokemonKoreaName) {
 }
 
 module.exports = setMakeLiTagPokemonList;
+
+
