b40c8b300cdf2c842913d6b2bccbe8b0a2cda5b4
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-23 20:58:46 +0900

    HTML 작성

---
+++ app.js
@@ -0,0 +1,3 @@
+// 내장모듈 불러오기
+const http = require('http');
+// pokemon 모듈 불러오기 (내가 만든 외부모듈 불러오기)
--- model/HTML-model.js
+++ model/HTML-model.js
@@ -4,3 +4,26 @@ const _pokemonKoreaName = require('./pokemon-package-model');
 const setMakeLiTagPokemonList = require('./make-li-tag-model');
 
 console.log(_pokemonKoreaName);
+console.log(typeof(setMakeLiTagPokemonList));
+// function
+
+const HTMLTemplate = `
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pokemon List</title>
+</head>
+<body>
+  <h1>Pokemon List</h1>
+  <ul>
+    ${setMakeLiTagPokemonList(_pokemonKoreaName)}
+  </ul>
+</body>
+</html>
+`;
+console.log(HTMLTemplate);
+
+module.exports = HTMLTemplate;
